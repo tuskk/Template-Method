5c3f5fc5de9178558e00f755ce4429b9c9e6876e
Mega <v.v.humeniuk@student.khai.edu>
2025-03-26 23:38:23 +0200

    21

---
+++ TemplateMethod/AbstractClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+abstract class AbstractClass
+{
+    public void TemplateMethod()
+    {
+        Step1();
+        Step2();
+        Step3();
+    }
+    protected abstract void Step1();
+    protected abstract void Step2();
+    protected abstract void Step3();
+}
